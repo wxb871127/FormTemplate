ec2c4a4578456dbf4231c478b5fbf668ac2ae121
y_zc <y_zc@winning.com.cn>
2019-12-26 10:03:29 +0800

    常用语修改为条目点击

--- template/src/main/java/template/widget/dialog/InputTemplateDialog.java
+++ template/src/main/java/template/widget/dialog/InputTemplateDialog.java
@@ -10,7 +10,9 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
 import android.widget.CursorAdapter;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 
@@ -25,26 +27,29 @@ import template.com.form.R;
 import template.view.MaxHeightListView;
 
 /**
- *   常用语弹出框
+ * 常用语弹出框
  */
-public class InputTemplateDialog extends BaseTemplateDialog<InputTemplate>{
+public class InputTemplateDialog extends BaseTemplateDialog<InputTemplate> {
     private View view;
     private TextView title;
     private MaxHeightListView listView;
     private Button btn_cancel;
     private Button btn_submit;
     private Adapter adapter;
-    private Map<String, String> map = new LinkedHashMap<>();;
+    private Map<String, String> map = new LinkedHashMap<>();
+    ;
+
     public InputTemplateDialog(Context mContext) {
         super(mContext);
     }
 
     OnInputDialogListener listener;
-    public interface OnInputDialogListener{
+
+    public interface OnInputDialogListener {
         void addQuote(String quote);
     }
 
-    public void setListener(OnInputDialogListener listener){
+    public void setListener(OnInputDialogListener listener) {
         this.listener = listener;
     }
 
@@ -52,21 +57,20 @@ public class InputTemplateDialog extends BaseTemplateDialog<InputTemplate>{
     public void initDialog(final InputTemplate template, Object value) {
         super.initDialog(template, value);
         view = LayoutInflater.from(mContext).inflate(R.layout.template_input_dialog, null);
-        title=view.findViewById(R.id.input_template_dialog_title);
-        listView =  view.findViewById(R.id.template_search_dialog_list);
+        title = view.findViewById(R.id.input_template_dialog_title);
+        listView = view.findViewById(R.id.template_search_dialog_list);
         btn_cancel = view.findViewById(R.id.btn_cancel);
         btn_submit = view.findViewById(R.id.btn_submit);
         adapter = new Adapter(mContext, null);
         listView.setListViewHeight(mContext.getResources().getDimensionPixelOffset(R.dimen.dp300));
         listView.setAdapter(adapter);
 
-        title.setText(template.label+"-常用语");
+        title.setText(template.label + "-常用语");
         ContentResolver contentResolver = mContext.getContentResolver();
         Cursor cursor = contentResolver.query(Uri.parse(template.uri), null,
-                template.primaryKey+" = ?" , new String[]{template.name}, null, null);
+                template.primaryKey + " = ?", new String[]{template.name}, null, null);
         cursor.moveToFirst();
         adapter.changeCursor(cursor);
-
 
 
         dialog = new Dialog(mContext);
@@ -75,7 +79,7 @@ public class InputTemplateDialog extends BaseTemplateDialog<InputTemplate>{
         btn_cancel.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(dialog != null)
+                if (dialog != null)
                     dialog.dismiss();
             }
         });
@@ -83,15 +87,15 @@ public class InputTemplateDialog extends BaseTemplateDialog<InputTemplate>{
         btn_submit.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if(listener != null) {
+                if (listener != null) {
                     StringBuilder builder = new StringBuilder();
-                    for(String key: map.keySet()){
+                    for (String key : map.keySet()) {
                         builder.append(key);
                     }
                     map.clear();
                     listener.addQuote(builder.toString());
                 }
-                if(dialog != null)
+                if (dialog != null)
                     dialog.dismiss();
             }
         });
@@ -110,24 +114,30 @@ public class InputTemplateDialog extends BaseTemplateDialog<InputTemplate>{
 
         @Override
         public void bindView(View view, Context context, Cursor cursor) {
+            LinearLayout layout = (LinearLayout) view.findViewById(R.id.ll);
             TextView tvText = (TextView) view.findViewById(R.id.template_search_dialog_item_text);
             final CheckBox checkBox = (CheckBox) view.findViewById(R.id.template_search_dialog_item_checkbox);
             final String showColumn = cursor.getString(cursor.getColumnIndex(template.showColumn));
             tvText.setText(showColumn);
             checkBox.setVisibility(View.VISIBLE);
+            checkBox.setOnCheckedChangeListener(null);
+            checkBox.setChecked(map.containsKey(showColumn));
 
-
-            checkBox.setOnClickListener(new View.OnClickListener() {
+            layout.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if(checkBox.isChecked()){
-                        map.put(showColumn, showColumn);
-                    }else {
-                        map.remove(showColumn);
-                    }
+                    checkBox.performClick();
                 }
             });
-
+            checkBox.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+                @Override
+                public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                    if (isChecked)
+                        map.put(showColumn, showColumn);
+                    else
+                        map.remove(showColumn);
+                }
+            });
         }
     }
 }
