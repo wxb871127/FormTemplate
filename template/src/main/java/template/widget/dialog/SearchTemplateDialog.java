56669cd740a0b80476eb3df5e59f43a9a82876d3
y_zc <y_zc@winning.com.cn>
2019-12-26 11:25:17 +0800

    搜索弹窗初步修改

--- template/src/main/java/template/widget/dialog/SearchTemplateDialog.java
+++ template/src/main/java/template/widget/dialog/SearchTemplateDialog.java
@@ -1,6 +1,6 @@
 package template.widget.dialog;
 
-import android.app.AlertDialog;
+import android.app.Dialog;
 import android.content.ContentResolver;
 import android.content.Context;
 import android.database.Cursor;
@@ -14,17 +14,19 @@ import android.widget.CheckBox;
 import android.widget.CursorAdapter;
 import android.widget.EditText;
 import android.widget.ImageView;
-import android.widget.ListView;
 import android.widget.TextView;
+
 import template.bean.SearchTemplate;
 import template.com.form.R;
+import template.view.MaxHeightListView;
 
-public class SearchTemplateDialog extends BaseTemplateDialog<SearchTemplate>{
+public class SearchTemplateDialog extends BaseTemplateDialog<SearchTemplate> {
     private View view;
     private EditText editText;
     private ImageView searchHint;
-    private ListView listView;
+    private MaxHeightListView listView;
     private Adapter adapter;
+    private TextView title;
 
     public SearchTemplateDialog(Context mContext) {
         super(mContext);
@@ -34,19 +36,21 @@ public class SearchTemplateDialog extends BaseTemplateDialog<SearchTemplate>{
     public void initDialog(final SearchTemplate template, Object value) {
         super.initDialog(template, value);
         view = LayoutInflater.from(mContext).inflate(R.layout.template_search_dialog, null);
+        TextView title = (TextView) view.findViewById(R.id.search_template_dialog_title);
         searchHint = view.findViewById(R.id.search_template_dialog_hint);
         editText = (EditText) view.findViewById(R.id.template_search_dialog_text);
-        listView = (ListView) view.findViewById(R.id.template_search_dialog_list);
+        listView = (MaxHeightListView) view.findViewById(R.id.template_search_dialog_list);
         adapter = new Adapter(mContext, null);
+        listView.setListViewHeight(mContext.getResources().getDimensionPixelOffset(R.dimen.dp300));
         listView.setAdapter(adapter);
-
-        if(editText.getTag() instanceof TextWatcher){//防止recyclerView刷新 触发TextWatcher事件
-            editText.removeTextChangedListener((TextWatcher)editText.getTag());
+        title.setText(template.label);
+        if (editText.getTag() instanceof TextWatcher) {//防止recyclerView刷新 触发TextWatcher事件
+            editText.removeTextChangedListener((TextWatcher) editText.getTag());
         }
-        if(value != null) {
+        if (value != null) {
             editText.setText(template.getShowName(value.toString(), mContext));
             searchHint.setVisibility(View.GONE);
-        }else
+        } else
             searchHint.setVisibility(View.VISIBLE);
 //            editText.setText(value.toString());
         TextWatcher textWatcher = new TextWatcher() {
@@ -66,12 +70,12 @@ public class SearchTemplateDialog extends BaseTemplateDialog<SearchTemplate>{
                     searchHint.setVisibility(View.GONE);
                     String selection = template.selection;
                     int count = 0;
-                    while(selection.indexOf("?")!=-1){
-                        selection = selection.substring(selection.indexOf("?")+1);
+                    while (selection.indexOf("?") != -1) {
+                        selection = selection.substring(selection.indexOf("?") + 1);
                         count++;
                     }
                     String[] selectionArg = new String[count];
-                    for(int i=0; i<count; i++)
+                    for (int i = 0; i < count; i++)
                         selectionArg[i] = s.toString();
 
                     ContentResolver contentResolver = mContext.getContentResolver();
@@ -79,17 +83,20 @@ public class SearchTemplateDialog extends BaseTemplateDialog<SearchTemplate>{
                             template.selection, selectionArg, null, null);
                     cursor.moveToFirst();
                     adapter.changeCursor(cursor);
-                }else
+                } else
                     searchHint.setVisibility(View.VISIBLE);
             }
         };
         editText.setTag(textWatcher);
         editText.addTextChangedListener(textWatcher);
-        dialog = new AlertDialog.Builder(mContext).setTitle(template.label)
-                .setView(view).create();
+
+        dialog = new Dialog(mContext);
+        dialog.setContentView(view);
+//        dialog = new AlertDialog.Builder(mContext).setTitle(template.label)
+//                .setView(view).create();
     }
 
-    public class Adapter extends CursorAdapter{
+    public class Adapter extends CursorAdapter {
 
         public Adapter(Context context, Cursor c) {
             super(context, c);
@@ -111,9 +118,9 @@ public class SearchTemplateDialog extends BaseTemplateDialog<SearchTemplate>{
             view.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View v) {
-                    if(listener != null)
+                    if (listener != null)
                         listener.onDataChanged(template, primaryKey, true);
-                    if(dialog != null)
+                    if (dialog != null)
                         dialog.dismiss();
                 }
             });
